d6101a9882e7c4dedf51e55a99adbb29b25c1af5
Dang Nguyen <dangns229@gmail.com>
2021-05-12 14:36:19 -0700

    create nestjs application

---
+++ spot-server/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule} from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({}),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
